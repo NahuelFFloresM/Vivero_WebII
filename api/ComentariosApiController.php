1ab7d39c3b047bc9d25eb209a05cb1281007bebb
MeyFonseca <mercedesvfonseca@gmail.com>
2020-11-28 20:55:26 -0300

    Modificacion en la función Agregar Comentarios

--- api/ComentariosApiController.php
+++ api/ComentariosApiController.php
@@ -8,10 +8,9 @@ class ComentariosApiController extends ApiController{
     private $model;
   
     public function __construct() {
-        // PREGUNTAR !!!!
         parent::__construct();
-        //
         $this->model = new ComentarioModel();
+
         session_start();
     }
 
@@ -20,28 +19,47 @@ class ComentariosApiController extends ApiController{
     }
 
     private function isAdmin(){
-        // Lo mejor seria consultar a la DB por el permiso
         if(isset($_SESSION["permisos"]) && ($_SESSION["permisos"] == 1)){
             return true;
         }
         return false;
     }
 
-    public function getComentarios($params =null) {
-        $comentarios = $this->model->getComentarios($params[0]);
-        if($comentario ==true){
-            return $this->response($comentario, 200);
-        }
-        else{
-            return $this->response(null, 404);
+    public function agregarComentario($params = null){
+        $array = $this->getData();
+        $comentario= $array->commentText;
+        $puntuacion=$array->inlineRadioOptions;
+        $id_producto=$array->id_producto;
+        //var_dump($puntuacion);
+        //die();
+        //if($array->comentario && $array->puntuacion){
+            $respuesta = $this->model->agregarComentario($comentario,$puntuacion,$id_producto,1);
+            if($respuesta>0){
+                $this->view->response("Se agrego exitosamente el comentario $respuesta",200);
+            }
+            else{
+                $this->view->response("No se pudo agregar el comentario",500);
+            }
+        //}
+    }
+    
+    public function getComentariosByUsers($params = null){
+        if ($this->isAdmin()){
+            $comentarios = $this->model->getComentariosByUsers();
+            $this->view->response($comentarios,200);
+        } else {
+            $this->view->response("El comentario no existe",404);
         }
     }
 
-    public function agregarComentario($params = null){
-        $array = $this->getData();
-        $respuesta = $this->model->agregarComentario($array->comentario, $array->puntuacion, $array->id_producto, $array->id_usuario);
-        
-        return $this->view->response($respuesta,200);
+    public function getComentarioById($params = null){
+        if ($this->isAdmin()){
+            $id = $params[':ID'];
+            $comentario = $this->model->getComentarioById($id);
+            $this->view->response($comentario,200);
+        } else {
+            $this->view->response("El comentario no existe",404);
+        }
     }
 
     public function borrarComentario($params = null){
@@ -54,36 +72,16 @@ class ComentariosApiController extends ApiController{
         }
     }
 
-    public function getComentariosByUsers(){
-        if ($this->isAdmin()){
-            $comentarios = $this->model->getComentariosByUsers();
-            $this->view->response($comentarios,200);
-        } else {
-            $this->view->response("Sesion Incorrecta",403);
+    public function getComentarios($params =null) {
+        $comentarios = $this->model->getComentarios($params[0]);
+        if($comentario ==true){
+            return $this->response($comentario, 200);
+        }
+        else{
+            return $this->response(null, 404);
         }
     }
 
-    public function getComentarioById($params = null){
-        if ($this->isAdmin()){
-            $id = $params[':ID'];
-            $comentario = $this->model->getComentarioById($id);
-            $this->view->response($comentario,200);
-        } else {
-            $this->view->response("Sesion Incorrecta",403);
-        }
-    }
-
-    public function editComentario(){
-        if ($this->isAdmin()){
-            $body = $this->getData();
-            $id = $body->id_comentario;
-            $nuevo_comentario = $body->comentario;
-            $comentario = $this->model->editComentario($nuevo_comentario,$id);
-            $this->view->response($comentario,200);
-        } else {
-            $this->view->response("Sesion Incorrecta",403);
-        }
-    }
-
+    
 
 }
